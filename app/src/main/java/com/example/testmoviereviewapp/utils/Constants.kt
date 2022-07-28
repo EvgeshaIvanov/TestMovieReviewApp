458688057cc71db8b15433908f76dd3e0015d6a1
Eugene Ivanov <most1360@yandex.ru>
2022-07-28 21:11:41 +0300

    commit #7

--- app/src/main/java/com/example/testmoviereviewapp/utils/Constants.kt
+++ app/src/main/java/com/example/testmoviereviewapp/utils/Constants.kt
@@ -1,11 +1,13 @@
 package com.example.testmoviereviewapp.utils
 
+import com.example.testmoviereviewapp.BuildConfig
+
 object Constants {
 
     const val BASE_URL = "https://api.nytimes.com/"
 
     const val REVIEWS_URL =
-        "svc/movies/v2/reviews/all.json?api-key=iyo1GlBaVN2AjrpB6uRFcTQzgO4ja4pN"
+        "svc/movies/v2/reviews/all.json?api-key=${BuildConfig.API_KEY}"
 
     const val PAGE = 0
 
